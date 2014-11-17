b1b3a19933f89e16804af47f15d7b19f2cff1d20
Sphiecoh <sfiso.dlaba@gmail.com>
2014-11-17 18:44:41 +0200

    Initial commit

---
+++ Sphiecoh.UniqueContraints.RavenDb/DocumentExistsException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sphiecoh.UniqueContraints.RavenDb
+{
+    class DocumentExistsException
+    {
+        //TODO : Create a custom exception
+    }
+}
---
+++ Sphiecoh.UniqueContraints.RavenDb/Program.cs
@@ -0,0 +1,63 @@
+﻿
+using Raven.Client.UniqueConstraints;
+using Raven.Database.Server;
+using Raven.Client.Document;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raven.Client;
+
+namespace Sphiecoh.UniqueContraints.RavenDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           using (var store = NewDocumentStore())
+			{
+				var list = new RavenUniqueEnforcer<User>();
+                list.AddProperty(x => x.Name);
+                list.AddProperty(x => x.Email);              		
+
+				using (var session = store.OpenSession())
+				{
+					//Should save
+					var user = new User() { Name = "John", Email = "john@gmail.com" };
+                    new RavenUniqueInserter().StoreUnique(session,user, list);
+
+                    //Should throw a ConcurrencyException exception 
+                    var user1 = new User() { Name = "John", Email = "john@gmail.com" };
+                    new RavenUniqueInserter().StoreUnique(session, user1, list);
+										
+				}
+			}
+
+		
+        }
+
+        private static IDocumentStore NewDocumentStore()
+        {
+          return  new DocumentStore
+            {
+                Url = "http://localhost:8085",
+                DefaultDatabase = "Test",
+
+            }.Initialize();
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+
+}
